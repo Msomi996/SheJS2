55b6b20b1dfcd42b41abbccc1790392055507d93
Msomi996 <nonhlemsomi9@gmail.com>
2023-08-26 06:55:01 +0200

    less 5

--- script.js
+++ script.js
@@ -3,3 +3,14 @@ alert("Junior developer");
 alert("pretty");
 let name = prompt("What is your name");
 alert("Nice to meet you " + name);
+let languageSpoken = prompt("which language do you speak?");
+
+if(languageSpoken === "Zulu") {
+    alert("Sawubona");
+}
+if(languageSpoken === "English") {
+    alert("Hello");
+}
+if(languageSpoken !== "Zulu") {
+    alert("Good day");
+}
