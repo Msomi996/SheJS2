5045da59801c2380e3b4d07c250fedee67059348
Msomi996 <nonhlemsomi9@gmail.com>
2023-09-03 03:09:14 +0200

    nonsense

---
+++ finalp.js
@@ -0,0 +1,7 @@
+// Add a fade-in animation to sections
+document.addEventListener("DOMContentLoaded", function () {
+    const sections = document.querySelectorAll("section");
+    sections.forEach((section) => {
+        section.classList.add("fade-in");
+    });
+});
